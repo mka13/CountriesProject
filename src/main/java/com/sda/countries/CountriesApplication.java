74d504ac3d7b503eb008b99a070a27aa6d88fd8d
Michal Kaczkowski <sirmichal23@gazeta.pl>
2018-12-12 19:02:43 +0100

    second commit

--- src/main/java/com/sda/countries/CountriesApplication.java
+++ src/main/java/com/sda/countries/CountriesApplication.java
@@ -8,7 +8,7 @@ public class CountriesApplication {
 
     public static void main(String[] args) {
 
-        System.out.println("Co tam slychac ?");
+        System.out.println("JAN ?");
         SpringApplication.run(CountriesApplication.class, args);
     }
 
